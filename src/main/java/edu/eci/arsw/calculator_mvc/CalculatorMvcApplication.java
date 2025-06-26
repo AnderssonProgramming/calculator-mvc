d1640014c4835a8c2501767968426a29018f25bf
AnderProgramming <158221956+AnderssonProgramming@users.noreply.github.com>
2025-06-26 18:56:03 -0500

    feat: add calculator-mvc project scaffolding

---
+++ src/main/java/edu/eci/arsw/calculator_mvc/CalculatorMvcApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.calculator_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/arsw/calculator_mvc/CalculatorMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.calculator_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
